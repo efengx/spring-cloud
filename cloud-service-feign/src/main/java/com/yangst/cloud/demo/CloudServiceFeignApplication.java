2107d67195c86f0101a0be4d4b6c95317234546c
yangst6857 <yangst@317hu.com>
2018-03-29 17:41:45 +0800

    优化

---
+++ cloud-service-feign/src/main/java/com/yangst/cloud/demo/CloudServiceFeignApplication.java
@@ -0,0 +1,12 @@
+package com.yangst.cloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServiceFeignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServiceFeignApplication.class, args);
+	}
+}
