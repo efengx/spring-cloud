3fe78ffe086f20e960a074a183f525849ba6445f
yangst6857 <yangst@317hu.com>
2018-03-29 10:01:05 +0800

    新项目

---
+++ cloud-server-eureka/src/main/java/com/yangst/cloud/demo/CloudServerEurekaApplication.java
@@ -0,0 +1,12 @@
+package com.yangst.cloud.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudServerEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudServerEurekaApplication.class, args);
+	}
+}
