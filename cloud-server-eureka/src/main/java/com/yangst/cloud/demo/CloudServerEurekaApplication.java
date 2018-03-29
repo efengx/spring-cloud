603fd6f19a87b86d8a15a40aca853bf05bc56d95
yangst6857 <yangst@317hu.com>
2018-03-29 10:10:08 +0800

    新项目

--- cloud-server-eureka/src/main/java/com/yangst/cloud/demo/CloudServerEurekaApplication.java
+++ cloud-server-eureka/src/main/java/com/yangst/cloud/demo/CloudServerEurekaApplication.java
@@ -2,7 +2,10 @@ package com.yangst.cloud.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+//启动一个服务注册中心提供给其他应用
+@EnableEurekaServer
 @SpringBootApplication
 public class CloudServerEurekaApplication {
 
